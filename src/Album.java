901789fd93aaba0f0ab3dff99dd58485a0885418
hammbino <hammondjeffrey@gmail.com>
2016-12-08 15:10:40 -0700

    Need to create arrays for Caption and file names. Initialize arrays. Compute correct position when clicking on thumbnail image. Implement file choose to save and open seriablizable albums. Thumbnail Scrolling is now working Album has been created. Caption lable is created and test data is loading.

--- src/Album.java
+++ src/Album.java
@@ -1,12 +1,14 @@
+/*
+ * Project6
+ * Created by Jeffrey Hammond on 11/29/16.
+ */
+
 import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-/**
- * Project6
- * Created by jeffreyhammond on 12/8/16.
- */
-public class Album implements Serializable {
+
+class Album implements Serializable {
 
     private ArrayList<File> files = new ArrayList<>();
     private ArrayList<String> fileNames = new ArrayList<>();
@@ -19,56 +21,52 @@ public class Album implements Serializable {
             files.add(fileURL);
         }
 
-        for (File fileName : files) {
-            fileNames.add(fileName.getName());
-        }
+        for (File fileName : files) fileNames.add(fileName.getName());
 
-        for (String caption : fileNames) {
-            fileCaption.add("");
-        }
+        for (String caption : fileNames) fileCaption.add("");
     }
 
-    public ArrayList<File> getFiles() {
+    ArrayList<File> getFiles() {
         return files;
     }
 
-    public int getAlbumSize() {
+    int getAlbumSize() {
         return files.size();
     }
 
-    public String getFileURL(int pos) {
+    String getFileURL(int pos) {
         return files.get(pos).getAbsolutePath();
     }
 
-    public File getFileAtLocation(int pos) {
+    File getFileAtLocation(int pos) {
         return files.get(pos);
     }
 
-    public ArrayList<String> getFileNames() {
+    ArrayList<String> getFileNames() {
         return fileNames;
     }
 
-    public String getName(int pos) {
+    String getName(int pos) {
         return fileNames.get(pos);
     }
 
-    public int getNameLocation(String name) {
+    int getNameLocation(String name) {
         return fileNames.indexOf(name);
     }
 
-    public String getFileCaption(int pos) {
+    String getFileCaption(int pos) {
         return fileCaption.get(pos);
     }
 
-    public void setFileCaption(String caption, int pos) {
+    void setFileCaption(String caption, int pos) {
         this.fileCaption.set(pos, caption);
     }
 
-    public void setPositionWhenSaved (int pos) {
+    void setPositionWhenSaved (int pos) {
         this.postionWhenSaved = pos;
     }
 
-    public int getPostionWhenSaved() {
+    int getPostionWhenSaved() {
         return postionWhenSaved;
     }
 
